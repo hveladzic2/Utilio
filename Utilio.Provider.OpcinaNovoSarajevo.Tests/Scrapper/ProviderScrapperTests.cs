e2809534d3c42ef17b2dd8096224ce5403d2f27c
Faris <44686174+fmehmedovi1@users.noreply.github.com>
2022-12-26 23:00:30 +0100

    Updated scrapper tests

--- Utilio.Provider.OpcinaNovoSarajevo.Tests/Scrapper/ProviderScrapperTests.cs
+++ Utilio.Provider.OpcinaNovoSarajevo.Tests/Scrapper/ProviderScrapperTests.cs
@@ -41,15 +41,20 @@ namespace Utilio.Provider.OpcinaNovoSarajevo.Tests.Scrapper
             var currentDirectory = testDirectory.Substring(0, testDirectory.Length - 17) + "/Scrapper/";
             var homepagePath = Path.Combine(currentDirectory, "homepage.html");
             var newsPath = Path.Combine(currentDirectory, "news.html");
+            var publicPurchasePath = Path.Combine(currentDirectory, "publicPurchase.html");
+            var publicPurchaseNewsPath = Path.Combine(currentDirectory, "publicPurchaseNews.html");
 
             string homepageData = File.ReadAllText(homepagePath);
             string newsData = File.ReadAllText(newsPath);
+            string publicPurchaseData = File.ReadAllText(publicPurchasePath);
+            string publicPurchaseNewsData = File.ReadAllText(publicPurchaseNewsPath);
 
 
             string homepageUrl = "https://novosarajevo.ba/o-opcini/sve-novosti/";
             string newsUrl =
                 "https://novosarajevo.ba/svecano-otvorene-nove-prostorije-pozajmnog-odjeljenja-biblioteka-sarajevo-u-ulici-paromlinska-35/";
-
+            string publicPurchaseUrl = "https://novosarajevo.ba/javne-nabavke/aktuelne-javne-nabavke/";
+            string publicPurchaseNewsUrl = "https://novosarajevo.ba/odluka-o-ponistenju-postupka-nabavka-i-postavljanje-podnih-obloga/";
 
             var mockedProtected = _msgHandler.Protected();
 
@@ -83,6 +88,25 @@ namespace Utilio.Provider.OpcinaNovoSarajevo.Tests.Scrapper
                 Content = new StringContent(newsData)
             });
 
+            mockedProtected.Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri!.Equals(publicPurchaseUrl)),
+                ItExpr.IsAny<CancellationToken>()
+            ).ReturnsAsync(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(publicPurchaseData)
+            });
+
+            mockedProtected.Setup<Task<HttpResponseMessage>>(
+                "SendAsync",
+                ItExpr.Is<HttpRequestMessage>(m => m.RequestUri!.Equals(publicPurchaseNewsUrl)),
+                ItExpr.IsAny<CancellationToken>()
+            ).ReturnsAsync(new HttpResponseMessage()
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(publicPurchaseNewsData)
+            });
 
             var fromDate = new DateTime(2022, 12, 15, 8, 20, 0);
             string lastReferenceIdentifier = "string";
@@ -93,10 +117,11 @@ namespace Utilio.Provider.OpcinaNovoSarajevo.Tests.Scrapper
 
             // Assert
             Assert.NotNull(content.entries);
-            Assert.Equal(1, content.entries.Count);
+            Assert.Equal(2, content.entries.Count);
             Assert.Equal("Svečano otvorene nove prostorije pozajmnog odjeljenja Biblioteka Sarajevo u ulici Paromlinska 35", content.entries.ElementAt(0).Title);
             Assert.Equal(new DateTime(2022, 12, 19), content.entries.ElementAt(0).PublishDate);
             Assert.Equal("o-opcini/sve-novosti/", content.entries.ElementAt(0).Description);
+            Assert.Equal("javne-nabavke/aktuelne-javne-nabavke/", content.entries.ElementAt(1).Description);
         }
     }
 }
